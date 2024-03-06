8f7f8ac7f8bc83425551887ce0692b87c0c760d2
Sergei <sergei.volodin1@ibm.com>
2024-03-06 13:11:49 +0100

    Config added

---
+++ config.py
@@ -0,0 +1,15 @@
+token = {
+    "scroll": {
+        "usdc": "0x06eFdBFf2a14a7c8E15944D1F4A48F9F95F663A4",
+        "usdt":"0xf55BEC9cafDbE8730f096Aa55dad6D22d44099Df"
+    },
+    "zksync": {
+        "usdc": "0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4",
+        "usdt": "0x493257fD37EDB34451f62EDf8D2a0C418852bA4C"
+    }
+}
+
+rpc = {
+    "scroll": "https://your_rpc_for_scrol",
+    "zksync": "https://your_rpc_for_zksync"
+}
